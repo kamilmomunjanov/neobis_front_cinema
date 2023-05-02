1eaf7537d1a359b9d6fafdfb690a76ebffebe57e
Kamil <kamilmomunjanov@gmail.com>
2023-05-02 21:19:52 +0600

    favorites start

--- js/main.js
+++ js/main.js
@@ -3,13 +3,33 @@
 const API_KEY = "f636e85f-179e-4dc1-851f-737d2ed75264";
 const API_URL_POPULAR = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1";
 const API_URL_SEARCH = "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
-const API_URL_TOP10 = "https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2023&month=MAY&limit=10"
+const API_URL_TOP_CURRENT_MONTH = "https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2023&month=MAY"
+const API_URL_TOP_EXPECTED_FILMS = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_AWAIT_FILMS&page=1"
+const API_URL_TOP_BEST_FILMS = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1"
+const API_URL_TOP_RELEASES = "https://kinopoiskapiunofficial.tech/api/v2.1/films/releases?year=2020&month=JANUARY&page=1"
+
 
 let favorites = []
 
+
+console.log(favorites)
 if (localStorage.getItem('favorites') !== null){
     favorites = JSON.parse(localStorage.getItem('favorites'))
 }
+
+document.querySelector(".header__favorite").addEventListener("click", (e) =>{
+    if (favorites !== null) {
+        const moviesEl =document.querySelector(".movies")
+        moviesEl.innerHTML = "";
+            const movieEl = document.createElement("div")
+            movieEl.classList.add("movie")
+        console.log(favorites.map((el) =>{
+            return el
+        }))
+    }else{
+        console.log("bad")
+    }
+})
 
 const headerLinks = document.querySelectorAll('.header__nav-link')
 
@@ -17,18 +37,21 @@ Array.from(headerLinks).forEach((item) => {
     item.addEventListener('click', () => {
         switch (item.id) {
             case 'month' : {
-                getMovies(API_URL_TOP10, 'items')
+                getMovies(API_URL_TOP_CURRENT_MONTH, 'items')
             }
             break
             case 'wait':{
-                getMovies()
+                getMovies(API_URL_TOP_EXPECTED_FILMS, 'films')
             }
             break
             case 'best':{
-                getMovies()
+                getMovies(API_URL_TOP_BEST_FILMS, "films")
             }
             break
             case 'release':{
+                getMovies(API_URL_TOP_RELEASES, "releases")
+            }
+            case 'favorite':{
                 getMovies()
             }
             break
@@ -48,7 +71,6 @@ async function getMovies(url, arr) {
     const respData = await resp.json()
     console.log(respData)
     await showMovies(respData, arr)
-    // topThisMonths(respData)
 }
 
 
@@ -74,7 +96,7 @@ function showMovies(data, arr) {
 
         movieEl.innerHTML = `
         <div class="movie__cover-inner">
-                        <img data-id="${el.filmId}"  class="movie__cover"
+                        <img class="movie__cover"
                              src="${el.posterUrlPreview}"
                              alt="${el.nameRu}">
                         <div class="movie__cover--darkened"></div>
@@ -83,14 +105,15 @@ function showMovies(data, arr) {
                         <div class="movie__title">${el.nameRu}</div>
                         <div class="movie__category">${el.genres.map((genre) => ` ${genre.genre}`)}</div>
                         ${el.rating && `<div class="movie__average movie__average--${getClassByRate(el.rating)}">${el.rating}</div>`}
-                        
+                        <div class="movie__heart"><button  data-id="${el.filmId}" class="movie__heart-btn" type="button">OK</button></div>
                         <div class="movie__year">${el.year}</div>
                     </div>
         `
         moviesEl.appendChild(movieEl)
     })
 
-    let favoritesBtn = document.querySelectorAll('.movie__cover')
+
+    let favoritesBtn = document.querySelectorAll('.movie__heart-btn')
 
     Array.from(favoritesBtn).forEach((item) => {
         item.addEventListener('click', () => {
@@ -105,30 +128,22 @@ function showMovies(data, arr) {
     })
 }
 
-// function topThisMonths(dataTop) {
-//     const moviesEl =document.querySelector(".movies")
+
+//     let favoritesBtn = document.querySelectorAll('.movie__cover')
 //
-//     document.querySelector(".movies").innerHTML = "";
-//     dataTop.items.forEach(element => {
-//         const movieEl = document.createElement("div")
-//         movieEl.classList.add("movie")
-//         movieEl.innerHTML = `
-//         <div class="movie__cover-inner">
-//                         <img class="movie__cover"
-//                              src="${element.posterUrlPreview}"
-//                              alt="${element.nameRu}">
-//                         <div class="movie__cover--darkened"></div>
-//                     </div>
-//                     <div class="movie__info">
-//                         <div class="movie__title">${element.nameRu}</div>
-//                         <div class="movie__category">${element.genres.map((genre) => ` ${genre.genre}`)}</div>
-//                         <div class="movie__average movie__average--${getClassByRate(element.rating)}">${element.rating}</div>
-//                         <div class="movie__year">${element.year}</div>
-//                     </div>
-//         `
-//         moviesEl.appendChild(movieEl)
+//     Array.from(favoritesBtn).forEach((item) => {
+//         item.addEventListener('click', () => {
+//             console.log(item.dataset)
+//             if (favorites.some(el => el.filmId == item.dataset.id)){
+//                 favorites = favorites.filter(el => el.filmId != item.dataset.id)
+//             } else {
+//                 favorites = [...favorites, data[arr].find(el => el.filmId == item.dataset.id)]
+//             }
+//             localStorage.setItem('favorites', JSON.stringify(favorites))
+//         })
 //     })
 // }
+
 
 const form = document.querySelector("form")
 const search = document.querySelector(".header__search")
